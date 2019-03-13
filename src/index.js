33cc5f0d0ba9d11f7505beb65c1f57ff3987ec2f
Andreas Goodstein <andreasgoodstein@gmail.com>
2019-03-13 23:07:33 +0100

    add sentry logging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { hydrate, render } from 'react-dom';
 
+import setupSentry from './vendor/sentry';
+
 import App from './app/app';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement.hasChildNodes()) {
   hydrate(App(), rootElement);
+  setupSentry();
 } else {
   render(App(), rootElement);
 }
